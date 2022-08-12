9f592da504d1c33fd88f76f6cc9aba0b3380057a
sjcahill <sjoscahill@gmail.com>
2022-08-11 22:40:30 -0400

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-fn main() {
-    println!("Hello, world!");
-    let x: i32
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0_u8; 1504];
+    let nbytes = nic.recv(&mut buf[..])?;
+    eprintln!("read {} bytes : {:x?}", nbytes, &buf[..nbytes]);
+    Ok(())
 }
