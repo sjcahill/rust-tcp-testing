7e351c67600a9cb3665cefd4355c128eca7663d3
Sean Cahill <sjoscahll@gmail.com>
2022-08-21 18:31:09 -0400

    update util script and modify .gitignore

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+cargo b --release 
+export CARGO_TARGET_DIR="/home/sj/Code/rust-tcp-testing/target"
+sudo setcap cap_net_admin=eip ${CARGO_TARGET_DIR}/release/thunder
+${CARGO_TARGET_DIR}/release/thunder &
+pid=$!
+sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip link set up dev tun0
+wait $pid
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ use std::io;
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0_u8; 1504];
-    let nbytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes : {:x?}", nbytes, &buf[..nbytes]);
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        eprintln!("read {} bytes : {:x?}", nbytes - 4, &buf[4..nbytes]);
+    }
     Ok(())
+
 }
