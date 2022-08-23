4d19f2116a9c9056f80238b67f4f248b74025968
Sean Cahill <sjoscahll@gmail.com>
2022-08-22 21:19:35 -0400

    Added Ipv4 and Tcp header parsing

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ ${CARGO_TARGET_DIR}/release/thunder &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,40 @@ fn main() -> io::Result<()> {
     let mut buf = [0_u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        eprintln!("read {} bytes : {:x?}", nbytes - 4, &buf[4..nbytes]);
-    }
-    Ok(())
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
+            // no ipv4
+            continue;
+        }
 
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                if proto != 0x06 {
+                    continue;
+                }
+
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4+p.slice().len()..]) {
+                    Ok(p) => {
+                        eprintln!(
+                            "{} - {} {} bytes of tcp to port: {} ",
+                            src,
+                            dst,
+                            p.slice().len(),
+                            p.destination_port()
+                        );
+                    }
+                    Err(e) => {
+                        eprintln!("Ignoring weird tcp packet {:?}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("ignoring weird packet {:?}", e)
+            }
+        }
+    }
 }
